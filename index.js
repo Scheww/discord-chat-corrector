f3d76982d9bee4100d333e5e02eafcb77883f786
Scheww <llucas1996@gmail.com>
2021-05-22 14:18:40 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("");
+
+client.on('message', message => {
+    const allowedChannels = ['music',  'musica', '『🎵』musica', 'música'];
+    const musicChannel = client.channels.cache.find(channel => allowedChannels.includes(channel.name.toLocaleLowerCase()));
+    const isMusicMessage = (msg) => msg.startsWith('!p ') || msg.startsWith('-p ') || msg.startsWith('p ');
+
+    if(!message.author.bot &&
+        isMusicMessage(message.content.toLocaleLowerCase()) &&
+        !allowedChannels.includes(message.channel.name.toLocaleLowerCase())
+    ) {
+        message.reply("Pone la musica donde se debe!")
+        musicChannel.send(message.content)
+        message.delete()
+    }
+});
+
+
