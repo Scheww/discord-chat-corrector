d77cf3dc3bd9d3a5c21d9670f4846ea9b388559a
lucho <lbuszmicz@gmail.com>
2021-09-28 19:14:12 -0300

    Beautify code

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ client.login("");
 client.on('message', message => {
     const allowedChannels = ['music',  'musica', '『🎵』musica', 'música'];
     const musicChannel = client.channels.cache.find(channel => allowedChannels.includes(channel.name.toLocaleLowerCase()));
-    const isMusicMessage = (msg) => msg.startsWith('!p ') || msg.startsWith('-p ') || msg.startsWith('p ');
+    const isMusicMessage = msg => msg.startsWith('!p ') || msg.startsWith('-p ') || msg.startsWith('p ');
 
     if(!message.author.bot &&
         isMusicMessage(message.content.toLocaleLowerCase()) &&
-        !allowedChannels.includes(message.channel.name.toLocaleLowerCase())
-    ) {
+        !allowedChannels.includes(message.channel.name.toLocaleLowerCase()))
+    {
         message.reply("Pone la musica donde se debe!")
         musicChannel.send(message.content)
         message.delete()
